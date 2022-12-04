afb8899a5b413743a7d41d069e01d27140516b03
Vitaly <poroshenkoe@gmail.com>
2022-12-04 16:16:24 +0300

    Add some functions like add new Todo, and delete Todo

--- src/App.js
+++ src/App.js
@@ -2,12 +2,29 @@ import { useState } from 'react';
 import './App.css';
 import FormChangeName from './components/FormChangeName';
 import ModalChangeNameList from './components/ModalChangeNameList';
-import { AiOutlineEdit } from 'react-icons/ai';
+import { AiOutlineEdit, AiOutlineCheck, AiFillDelete } from 'react-icons/ai';
 
 function App() {
   const [nameOfMain, setNameOfMain] = useState('Todo KazanExpress');
   const [inputName, setInputName] = useState('');
-  const [show, setShow] = useState(false);
+  const [show, setShow] = useState(false); // Show modal window
+  const [newTodo, setNewTodo] = useState(''); // input add todo in list
+  const [todos, setTodos] = useState([]); // list todo
+
+  function deleteTodo(x) {
+    console.log(x);
+    setTodos(todos.filter((p) => p.id !== x.id));
+  }
+
+  function addNewTodo() {
+    const todoList = {
+      id: Date.now(),
+      newTodo,
+    };
+
+    setTodos([...todos, todoList]);
+    setNewTodo('');
+  }
 
   function changeInputName(e) {
     e.preventDefault();
@@ -34,6 +51,28 @@ function App() {
             changeInputName={changeInputName}
           />
         </ModalChangeNameList>
+      </div>
+
+      <div className="div-input">
+        <input
+          className="main-input"
+          placeholder="Add some Todo"
+          value={newTodo}
+          onChange={(e) => setNewTodo(e.target.value)}
+        ></input>
+        <button onClick={addNewTodo}>Add</button>
+      </div>
+      <div>
+        {todos.map((value, index) => (
+          <div key={value.id}>
+            <div className="todoList">{`${index + 1}. ${value.newTodo} `}</div>
+            <div className="icons">
+              <AiOutlineCheck />
+              <AiOutlineEdit />
+              <AiFillDelete onClick={() => deleteTodo(value)} />
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
