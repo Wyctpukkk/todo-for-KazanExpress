e831417e4f45383660f998483d10829c6246bda8
Vitaly <poroshenkoe@gmail.com>
2022-12-04 23:43:33 +0300

    add todo done

--- src/App.js
+++ src/App.js
@@ -11,15 +11,25 @@ function App() {
   const [newTodo, setNewTodo] = useState(''); // input add todo in list
   const [todos, setTodos] = useState([]); // list todo
 
-  function deleteTodo(x) {
-    console.log(x);
-    setTodos(todos.filter((p) => p.id !== x.id));
+  function deleteTodo(currentTodoList) {
+    setTodos(todos.filter((todoList) => todoList.id !== currentTodoList.id));
+  }
+
+  function checked(currentTodoList) {
+    setTodos(
+      todos.map((todoList) => {
+        return currentTodoList.id === todoList.id
+          ? { ...todoList, isDone: !todoList.isDone }
+          : { ...todoList };
+      })
+    );
   }
 
   function addNewTodo() {
     const todoList = {
       id: Date.now(),
       newTodo,
+      isDone: false,
     };
 
     setTodos([...todos, todoList]);
@@ -63,16 +73,22 @@ function App() {
         <button onClick={addNewTodo}>Add</button>
       </div>
       <div>
-        {todos.map((value, index) => (
-          <div key={value.id}>
-            <div className="todoList">{`${index + 1}. ${value.newTodo} `}</div>
-            <div className="icons">
-              <AiOutlineCheck />
-              <AiOutlineEdit />
-              <AiFillDelete onClick={() => deleteTodo(value)} />
+        {todos.length < 1 ? (
+          <div>For create todo push Add </div>
+        ) : (
+          todos.map((todoList, index) => (
+            <div key={todoList.id}>
+              <div
+                className={todoList.isDone ? 'todoList active' : 'todoList'}
+              >{`${index + 1}. ${todoList.newTodo} `}</div>
+              <div className="icons">
+                <AiOutlineCheck onClick={() => checked(todoList)} />
+                <AiOutlineEdit />
+                <AiFillDelete onClick={() => deleteTodo(todoList)} />
+              </div>
             </div>
-          </div>
-        ))}
+          ))
+        )}
       </div>
     </div>
   );
